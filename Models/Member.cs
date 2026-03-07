dc622eeefcf5ca51a60cca81fd3146e08bce0186
IgnTorn <tornikeebralidzevar@gmail.com>
2026-03-07 14:01:43 +0400

    Book, BookService, Member, and MemberService is in order.

---
+++ Models/Member.cs
@@ -0,0 +1,23 @@
+﻿public class Member
+{
+    public int Id;
+    public string FirstName;
+    public string LastName;
+    public int Age;
+    public string Email;
+    public string PersonalNumber;
+    public DateTime CreatedOn;
+
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Id: {Id}");
+        Console.WriteLine($"Name: {FirstName} {LastName}");
+        Console.WriteLine($"Age: {Age}");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"PersonalNumber: {PersonalNumber}");
+        Console.WriteLine("CreatedOn: " + CreatedOn.Date.ToShortDateString());
+        Console.WriteLine("----------------------");
+    }
+}
+
